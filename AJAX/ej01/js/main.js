0ed9de1a6d5b29664de9735b8477b2fa7a3be846
DAW <daw@daw-VirtualBox.(none)>
2014-03-20 19:23:07 +0100

    Ejercicios AJAX jQuery

---
+++ AJAX/ej01/js/main.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+	'use strict';
+
+	//Asignar la direccion actual de la pagina al cuadro de texto del recurso
+	$('#recurso').val(document.URL);
+
+	//añadir evento al boton de mostrar contenido
+
+	var mostrarContenido = function(datos){
+
+		$('#contenidos').text(datos);
+	};
+
+	var mostrarCabezera = function(datos){
+		$('#cabeceras').text(datos);
+	};
+	var mostrarEstados = function(status, textStatus){
+		$('#codigo').text(status + ' ' + textStatus);
+	};
+
+	var peticionAjax = function(){
+
+		$.ajax({
+			url : $('#recurso').val(),
+			dataType : 'text',
+			cache : false,
+			success : function(data, textStatus, jqXHR){
+				console.log('exito');
+				mostrarContenido(data);
+				mostrarCabezera(jqXHR.getAllResponseHeaders());
+				mostrarEstados(jqXHR.status , jqXHR.statusText);
+
+			},
+			error : function(jqXHR, textStatus, errorThrown){
+				console.log(errorThrown);
+			},
+
+		});
+	};
+
+	$(document).on('click', 'input[value="Mostrar contenidos"]', peticionAjax);
+	//$(document).on('submit', 'form', peticionAjax);
+
+
+});
---
+++ AJAX/ej02/cliente/js/main.js
@@ -0,0 +1,70 @@
+$(document).ready(function(){
+	'use strict';
+	var $ticker = $('#ticker');
+
+	$ticker.data({
+		'activo' : true,
+		'noticias' : {},
+		'noticiaActual': 0
+
+	});
+
+	var mostrarNoticiaNueva = function(data){
+
+		var hora = new Date();
+		var mensaje = hora.getHours() + ':' +
+			hora.getMinutes() + ':' +
+			hora.getSeconds() + ' ' + data;
+		$ticker.text(mensaje);
+		$ticker.data('noticias').push(mensaje);
+
+	};
+
+	var mostrarAnterior = function(){
+		cambiarEstado();
+		var noticiaActual = ticker.data('noticiaActual');
+		if(noticiaActual > 0){
+			var mensaje = ticker.data('noticias')[noticiaActual];
+			ticker.data('noticiaActual', ++noticiaActual);
+			$ticker.text(mensaje);
+		}
+	};
+
+	var pedirNoticia = function(){
+
+		$.ajax({
+			url : '../servidor/generaContenidos.php',
+			dataType : 'text',
+			cache : false,
+			success : function(data, textStatus, jqXHR){
+				mostrarNoticiaNueva(data);
+
+			},
+			error : function(jqXHR, textStatus, errorThrown){
+				console.log(errorThrown);
+			},
+
+		});
+
+	};
+
+
+	//var activo = true;
+	var intervalo = setInterval(pedirNoticia, 1000);
+
+	var cambiarEstado = function(){
+		var activo = $ticker.data('activo');
+		if(activo){
+			$ticker.data('activo', false);
+			clearInterval(intervalo);
+		}else{
+			$ticker.data('activo', true);
+			intervalo = setInterval(pedirNoticia, 1000);
+		}
+
+	};
+	$(document).on('click', 'input[id=detener]',cambiarEstado);
+
+
+
+});
