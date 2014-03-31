175cf2f8dff196617937398bb423e4adf5ffe9dc
Fracisco Recio <daw@daw-VirtualBox.(none)>
2014-03-31 18:29:35 +0200

    Ejercicio 02

---
+++ HTML5/ej02/js/main.js
@@ -0,0 +1,27 @@
+
+$(document).ready(function(){
+	"use strict";
+
+	var contenido = localStorage.getItem('contenido')||"El contenido de este bloque es editable";
+
+	$('#editable').designMode = 'on';
+	$('#editable').html(contenido);
+
+
+	$(document).on('click','#negrita',function(e){
+		document.execCommand('bold',false,null);
+
+	});
+	$(document).on('click','#cursiva',function(e){
+		document.execCommand('italic',false,null);
+
+	});
+	$(document).on('click','#sub',function(e){
+		document.execCommand('underline',false,null);
+
+	});
+
+	$(document).on('click','#guardar',function(e){
+		localStorage.setItem('contenido', $('#editable').html());
+	});
+});
