6ddda88d3b420893792b6e95262c099f0ca7627f
Fracisco Recio <daw@daw-VirtualBox.(none)>
2014-04-07 18:13:23 +0200

    Ejercicio IndexDB

--- HTML5/ej11/js/main.js
+++ HTML5/ej11/js/main.js
@@ -3,6 +3,7 @@ window.indexedDB = window.indexedDB || window.mozIndexedDB ||
 
 window.IDBTransaction = window.IDBTransaction ||
                 window.webkitIDBTransaction || window.msIDBTransaction;
+
 window.IDBKeyRange = window.IDBKeyRange ||
                 window.webkitIDBKeyRange || window.msIDBKeyRange;
 
@@ -16,6 +17,7 @@ function open () {
     var version = 4;
     var request = indexedDB.open("todo-list", version);
 
+
     request.onupgradeneeded = function(e) {
         db = e.target.result;
 
@@ -26,7 +28,9 @@ function open () {
     request.onerror = onerror;
 
     request.onsuccess = function(e) {
+        db = e.target.result;
         console.log("DB opened");
+        getAllTodoItems();
     };
 }
 
@@ -42,12 +46,16 @@ function add (todoText) {
     var request = store.put(data);
 
     request.onsuccess = function(e) {
-        console.log("Sucessful add: "+e);
+        console.log("Sucessful add: " + e);
     };
 
     request.onerror = function(e) {
         console.log("Error adding: ", e);
     };
+
+    store.transaction.oncomplete = function(e){
+        getAllTodoItems();
+    }
 }
 
 function getAllTodoItems() {
@@ -62,22 +70,52 @@ function getAllTodoItems() {
     cursorRequest.onsuccess = function(e) {
         var result = e.target.result;
         if(!!result == false) return;
-
         console.log(result.value)
+        todos.innerHTML+=
+        '<li data-timestamp="' + result.value.timeStamp + '">' + result.value.text + '</li>';
         result.continue();
     };
 
     cursorRequest.onerror = onerror;
 };
 
+function del(timestamp){
+    var transaction = db.transaction(["todo-list"], "readwrite");
+    var store = transaction.objectStore("todo-list");
+
+    var request = store.delete(parseInt(timestamp));
+
+    request.onsuccess = function(e){
+        console.log("Deleted");
+    };
+
+    request.onerror = onerror;
+
+    store.transaction.oncomplete = function(e){
+        getAllTodoItems();
+    }
+
+
+};
+
 function addTodo() {
     var todo = document.getElementById("todo");
     add(todo.value);
     todo.value = "";
-}
+};
+
+var deleteTodo = function(e) {
+    console.log(this);
+    console.log(this.dataset.timestamp);
+    del(this.dataset.timestamp);
+};
 
 function init() {
     open();
+
 }
 
 window.addEventListener("DOMContentLoaded", init, false);
+
+$(document).on('click', '#todoItems li', deleteTodo);
+
