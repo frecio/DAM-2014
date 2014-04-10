19c36540be52e7a2496023906fa711e9c1f48c2c
Fracisco Recio <daw@daw-VirtualBox.(none)>
2014-04-10 18:59:16 +0200

    formularios y eventos

---
+++ JQueryMobile/ej01/js/main.js
@@ -0,0 +1,36 @@
+$('#index').on('orientationchange', function(e){
+	alert('has girado el dispositivo');
+});
+
+$('#index').on('tap', function(e){
+	console.log('tap');
+	alert('tap');
+});
+
+$('#index').on('taphold', function(e){
+	console.log('taphold');
+});
+
+$('#index').on('swipe', function(e){
+	console.log('swipe');
+});
+
+$('#index').on('swipeleft', function(e){
+	console.log('swipeleft');
+});
+
+$('#index').on('swiperight', function(e){
+	console.log('swiperight');
+});
+
+$('#index').on('scrollstart', function(e){
+	console.log('scrollstart');
+});
+
+$('#index').on('scrollstop', function(e){
+	console.log('scrollstop');
+});
+
+
+
+
---
+++ JQueryMobile/ej01/js/multiple.js
@@ -0,0 +1,56 @@
+//Page events
+$('#page1','#page2').on('pagebeforeload', function(e){
+	console.log('pagebeforeload');
+});
+
+$('#page1','#page2').on('pagecontainerload', function(e){
+	console.log('pagecontainerload');
+});
+
+//Page change events
+$('#page1','#page2').on('pagebeforechange', function(e){
+	console.log('pagebeforechange');
+});
+
+$('#page1','#page2').on('pagechange', function(e){
+	console.log('pagechange');
+});
+
+//Page transition events
+$('#page1','#page2').on('pagebeforeshow', function(e){
+	console.log('pagebeforeshow');
+});
+
+$('#page1','#page2').on('pagebeforehide', function(e){
+	console.log('pagebeforehide');
+});
+
+$('#page1','#page2').on('pageshow', function(e){
+	console.log('pageshow');
+});
+
+$('#page1, #page2').on('pagehide', function(e){
+	console.log('pagehide');
+});
+
+
+//Page initialization events
+$('#page1, #page2').on('pagebeforecreate', function(e){
+	console.log('pagebeforecreate');
+});
+$('#page1, #page2').on('pagecreate', function(e){
+	console.log('pagecreate');
+});
+$('#page1, #page2').on('pageinit', function(e){
+	console.log('pageinit');
+});
+
+//Page remove events
+$('#page1, #page2').on('pageremove', function(e){
+	console.log('pageremove');
+});
+
+$('#index, #page1','#page2').on('tap', function(e){
+	console.log('tap');
+	alert('tap');
+});
